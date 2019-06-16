4c6181121780b76f22e229be96fb928d1679495b
Clayton Cook <claytoncook@Claytons-MacBook-Pro-2.local>
2019-06-15 23:57:52 -0400

    📊 Basic Functionailty and UI ⚙️

--- main.py
+++ main.py
@@ -2,29 +2,48 @@ from tkinter import *
 from tkinter import filedialog
 import os
 
+os.system("clear")
+
 app = Tk()
 direct = StringVar()
+files = []
+formats = ["jpeg","tiff","png","gif","jp2","pict","bmp","qtif","psd","sgi","tga"]
+v = StringVar()
+v.set("jpeg")
 
 def selectDirectory():
     app.filename = filedialog.askdirectory()
-    print(os.listdir(app.filename))
+    print(app.filename)
+    files.append(os.listdir(str(app.filename)))
     direct.set("Directory: " + str(app.filename))
+    os.chdir(str(app.filename))
+    os.system("ls")
 
-def displayFiles():
-    print(os.path.exists("Output"))
-
-def init():
-    if os.path.exists("Output") != True :
-        print("Creating Ouput Directory...")
-        os.mkdir("Output")
-    else:
-        print("Ouput Directory Already Exists!")
+def reformat():
+    print(v.get())
+    for f in files[0]:
+        data = f.split(".")
+        print(f)
+        os.system("sips -s format " + v.get() + " " + f + " --out " + data[0] + "." + v.get())
+        os.remove(f)
 
 def build():
-    init()
-    Button(app, text="Select Directory", command=selectDirectory).pack(side=LEFT)
+    chooseDirectoryFrame = Frame(app)
+    chooseDirectoryFrame.pack(side=TOP)
+    Button(chooseDirectoryFrame, text="Select Directory", command=selectDirectory).pack(side=LEFT)
     direct.set("Directory: ")
-    Label(app, textvariable=direct).pack(side=LEFT)
+    Label(chooseDirectoryFrame, textvariable=direct).pack(side=LEFT)
+
+    imageFormatOptionsFrame = Frame(app)
+    imageFormatOptionsFrame.pack()
+    for text in formats:
+        Radiobutton(imageFormatOptionsFrame, text=text, variable=v, value=text).pack(side=LEFT)
+
+    submitFrame = Frame(app)
+    submitFrame.pack(side=BOTTOM)
+    Button(submitFrame, text="Reformat", command=reformat).pack(side=LEFT)
+
+    app.title("Image Reformatter")
     app.mainloop()
 
 build()
